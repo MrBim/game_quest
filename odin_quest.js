b4533243eb4ca00bd1b2bc53a255a1364405be3b
MrBim <bimtech84@gmail.com>
2017-06-16 22:23:04 +0100

    inital commit

---
+++ odin_quest.js
@@ -0,0 +1,110 @@
+
+// keyboard
+var keys = [];
+
+// canvas variables
+var width = 1000;
+var height = 700; 
+
+// canvas
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// event listeners
+document.body.addEventListener("keydown", function(e) {
+	keys[e.keyCode] = true;
+});
+
+document.body.addEventListener("keyup", function(e) {
+	keys[e.keyCode] = false;
+});
+
+// game elements
+var hasRun = false;
+
+
+function drawCanvas() {
+	ctx.clearRect(0,0,width,height);
+}
+
+function thor_movement(){
+	// up (w)
+	if (keys[87]) {
+		isPointing = 1;
+		yPos -= moveSize;
+		if( yPos <= 0){
+			yPos = 0;
+		}
+		walkAnimFrame += 1;
+	}
+	// down (s)
+	if (keys[83]) {
+		isPointing = 3;
+		yPos += moveSize;
+		if( yPos >= height - dispSize){
+			yPos = height - dispSize;
+		}
+		walkAnimFrame += 1;
+	}    
+	// left (a)
+	if (keys[65]) {
+		isPointing = 2;
+		xPos -= moveSize;
+		if( xPos <= 0){
+			xPos = 0;
+		}
+		walkAnimFrame += 1;
+	}
+	// right (d)
+	if (keys[68]) {
+		isPointing = 4;
+		xPos += moveSize;
+		if( xPos >= width - dispSize){
+			xPos = width - dispSize;
+		}
+		walkAnimFrame += 1;
+	}
+}
+function drawPlayer() { // draw player as a square
+	drawCanvas();
+	ctx.fillStyle = "#000000";
+	ctx.fillRect(xPos, yPos,dispSize,dispSize);
+	ctx.fill();	
+}
+
+function quit() {
+	hasRun = false;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	xPos = ((width/2) - (dispSize/2));
+	yPos = ((height/2) - (dispSize/2));
+}
+
+function stopMusic(){
+	gameMusic.pause();
+}
+
+//------------------ gameloop ---------------------------------------------------------
+function gameLoop(){
+	
+	if (hasRun === false) {
+		// initalise all game variables here
+		drawCanvas();	
+		gameMusics.play();	
+		hasRun = true;	
+	}
+	
+	if (lives === 0) {
+		quit();
+	}
+
+	drawCanvas();
+ 	thor_movement();
+ 	drawPlayer();
+
+ 	requestAnimationFrame(gameLoop);
+
+	// 'q' for quit
+	if (keys[81]) {  	
+		quit();
+	}
+}
---
+++ readme.md
@@ -0,0 +1,26 @@
+Odin Quest
+
+this is a little top down adventure game featuring VIKINGS.
+its going to look pretty blocky
+
+it is basicly an investigation in to using the html canvas and javascript
+
+therefore i havent used any packages or librarys. 
+
+
+current controlls are 
+
+direction buttons:- move the player in their respective directions 
+
+ctrl:- is the action button (currently this is for going thru doors but i have more plans for it in the future) 
+
+space:- fires lightening (use you imagination that yellow square is thor's powerful lightening, trust me)
+
+
+you should get points for killing the ice giants (at the moment you also get points for throwing lightening)
+
+ice giants respawn when you go in to a different room
+
+there is a button to mute the horrible music
+
+q respawns a new ice giant and resets the score as well as re enabling the horrible music.
